f9c5c97dad0e1b809e55d6ab169eed7885b177c9
Sholpan Bakhtybekkyzy <pasadena2929@gmail.com>
2023-03-21 19:08:43 +0600

    Добавление страницы регистрации

---
+++ accounts/forms.py
@@ -0,0 +1,30 @@
+from django import forms
+from django.contrib.auth.models import User
+
+
+class CustomUserCreationForm(forms.ModelForm):
+    password = forms.CharField(label='Пароль', strip=False, required=True, widget=forms.PasswordInput)
+    password_confirm = forms.CharField(label='Подтвердите пароль', strip=False, required=True,
+                                       widget=forms.PasswordInput)
+
+    def clean(self):
+        cleaned_data = super().clean()
+        password = cleaned_data.get('password')
+        password_confirm = cleaned_data.get('password_confirm')
+        if password and password_confirm and password != password_confirm:
+            raise forms.ValidationError('Пароли не совпадают!')
+        first_name = cleaned_data.get('first_name')
+        last_name = cleaned_data.get('last_name')
+        if not first_name and not last_name:
+            raise forms.ValidationError('Укажите хотя бы одно из полей "Имя" или "Фамилия"')
+
+    def save(self, commit=True):
+        user = super().save(commit=False)
+        user.set_password(self.cleaned_data.get('password'))
+        if commit:
+            user.save()
+        return user
+
+    class Meta:
+        model = User
+        fields = ['username', 'password', 'password_confirm', 'first_name', 'last_name', 'email']
--- accounts/views.py
+++ accounts/views.py
@@ -1,5 +1,10 @@
 from django.contrib.auth import authenticate, login, logout
+from django.contrib.auth.models import User
 from django.shortcuts import render, redirect
+from django.urls import reverse
+from django.views.generic import CreateView
+
+from accounts.forms import CustomUserCreationForm
 
 
 def login_view(request):
@@ -19,3 +24,33 @@ def login_view(request):
 def logout_view(request):
     logout(request)
     return redirect('index')
+
+
+class RegisterView(CreateView):
+    model = User
+    template_name = 'user_create.html'
+    form_class = CustomUserCreationForm
+
+    def form_valid(self, form):
+        user = form.save()
+        login(self.request, user)
+        return redirect(self.get_success_url())
+
+    def get_success_url(self):
+        next_url = self.request.GET.get('next')
+        if not next_url:
+            next_url = self.request.POST.get('next')
+        if not next_url:
+            next_url = reverse('index')
+        return next_url
+
+    # success_url = '/'
+    #
+    # def post(self, request, *args, **kwargs):
+    #     form = self.form_class(request.POST)
+    #     if form.is_valid():
+    #         user = form.save()
+    #         login(self.request, user)
+    #         return redirect(self.success_url)
+    #     context = {'form': form}
+    #     return self.render_to_response(context)
